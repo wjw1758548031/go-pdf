bdd4de6724886950991b9c0fdc2add9a7f2dee40
wangjianwen <wjw1758548031@163.com>
2019-12-05 13:41:28 +0800

    delete

--- unipdf/internal/jbig2/bitmap/doc.go
+++
@@ -1,8 +0,0 @@
-/*
- * This file is subject to the terms and conditions defined in
- * file 'LICENSE.md', which is part of this source code package.
- */
-
-// Package bitmap contains the bitmap data container for the
-// binary images used when decoding the jbig2 encoded images.
-package bitmap
